f6998c10c93d44cb0cf8b58989419aea66e9d86e
OsborneSebuabe <ojsebuabe@st.ug.edu.gh>
2024-06-02 19:02:25 +0000

    Why is github unnecessarily complex?

---
+++ Grade Calculator/Grade Calculator/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class GradeConverter
+{
+    static void Main()
+    {
+        Console.Write("Enter a numerical grade between 0 and 100: ");
+        int grade =
+            Convert.ToInt32(Console.ReadLine());
+
+        if (grade >= 90) 
+        {
+            Console.WriteLine("The letter grade is A");
+        }
+        else if (grade >= 80)
+        {
+            Console.WriteLine("The letter grade is B");
+        }
+        else if (grade >= 70)
+        {
+            Console.WriteLine("The letter grade is C");
+        }
+        else if (grade >= 60)
+        {
+            Console.WriteLine("The letter grade is D");
+        }
+        else 
+        {
+            Console.WriteLine("The letter grade is F");
+        }
+    }
+}
+
---
+++ Ticket Price Calculator/Ticket Price Calculator/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class TixketPriceCalculator
+{
+    static void Main()
+    {
+        const int standardPrice = 10;
+        const int discountedPrice = 7;
+
+        Console.Write("Please enter your age: ");
+            int age = Convert.ToInt32(Console.ReadLine());
+
+        int ticketPrice;
+
+        if (age >= 65 || age <=12)
+        {
+            ticketPrice = discountedPrice;
+        }
+        else { ticketPrice = standardPrice;}
+
+        Console.WriteLine($"The ticket price is: GHC{ticketPrice}");
+    }
+}
+
